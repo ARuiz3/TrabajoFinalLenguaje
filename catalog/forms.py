2efcdef8f4a4fc320b5118466d08a8a66dd253b3
ARuiz3 <aruiz2003b@gmail.com>
2023-05-17 20:12:12 +0200

    es la hora

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,11 +1,13 @@
 import datetime
+from catalog.models import BookInstance
 
 from django import forms
+from django.forms import ModelForm
 
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
-class RenewBookForm(forms.Form):
+class RenewBookModelForm(ModelForm):
     renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
 
     def clean_renewal_date(self):
@@ -21,6 +23,13 @@ class RenewBookForm(forms.Form):
 
         # Remember to always return the cleaned data.
         return data
+    
+    class Meta:
+        model = BookInstance
+        fields = ['due_back']
+        labels = {'due_back':_('Renewal date')}
+        help_texts = {'due_back':_('Enter a date between now and 4 weeks (default 3).')}
+
 
 
 class CartAddBookForm(forms.Form):
